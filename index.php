1d91d8e432650b3404ca9862655215c28b837644
harris <harisnovrial29@gmail.com>
2018-04-07 17:13:58 +0700

    kurang di atur kerapiannya

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
         <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+            <!-- navbar -->
+         <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
                 <a>
                     <img src="bird.jpg" alt="logo" style="width:40px;">
                 </a>
@@ -42,12 +42,12 @@
             </div>
         </nav>
 
-
+          <!-- Tampilan home   -->
         <div class="container-fluid" id="home">
             <div class="container" id="kartu">
                 <div class="row" >
                     <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <div class="card" >
+                        <div class="card kartu1" >
                             <div class="card-body">
                                 <h1 style="text-align :center">ANGKEL</h1>
                                 <h2 style="text-align :center">Angkutan & Bengkel</h2>
@@ -57,6 +57,133 @@
                 </div>
             </div>
         </div>
+
+        <!-- tampilan Jadwal -->
+
+        <div class="container" id="timeline ">
+            <div class="page-header">
+                <h1 style="text-align :center"  >Jadwal Servis bus</h1>
+            </div>
+            <ul class="timeline">
+                <li>
+                <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                    <h4 class="timeline-title">Hello</h4>
+                    <p></p>
+                    </div>
+                    <div class="timeline-body">
+                    <p></p>
+                    </div>
+                </div>
+                </li>
+                <li class="timeline-inverted">
+                <div class="timeline-badge warning"><i class="glyphicon glyphicon-credit-card"></i></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                    <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                    </div>
+                    <div class="timeline-body">
+                    <p></p>
+                    </div>
+                </div>
+                </li>
+                <li>
+                <div class="timeline-badge danger"><i class="glyphicon glyphicon-credit-card"></i></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                    <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                    </div>
+                    <div class="timeline-body">
+                    <p></p>
+                    </div>
+                </div>
+                </li>
+                <li class="timeline-inverted">
+                <div class="timeline-badge warning"><i class="glyphicon glyphicon-credit-card"></i></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                    <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                    </div>
+                    <div class="timeline-body">
+                    <p></p>
+                    <p></p>
+                    </div>
+                </div>
+                </li>
+            </ul>
+        </div>
+
+        <!-- TAMPILAN ANGKEL -->
+        <div class="container-fluid" id="angkel">
+            <div class="container" >
+            <h1 style="text-align :center">Angkel</h1>
+                <div class="row" >
+                    <div class="col center " id="jajal">
+                        <div class="card kartu2" style="width:400px">
+                                <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+                                <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                                </div>
+                        </div>
+                    </div>
+                        <div class="col center" id="jajal2">
+                            <div class="card kartu2" style="width:400px; height:400px;" >
+                                <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+                                <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Tampilan aboutUS -->
+        <div class="container-fluid" id="#">
+            <div class="container" >
+            <h1 style="text-align :center">About US</h1>
+                <div class="row" >
+                    <div class="col center " id="jajal">
+                        <div class="card kartu2" style="width:400px">
+                                <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+                                <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                                </div>
+                        </div>
+                    </div>
+                        <div class="col center" id="jajal2">
+                            <div class="card kartu2" style="width:400px; height:400px;" >
+                                <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+                                <div class="card-body">
+                                <h4 class="card-title">John Doe</h4>
+                                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                                <a href="#" class="btn btn-primary">See Profile</a>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    
+        <!-- Tampilan contact -->
+
+        <div class="container-fluid" id="aboutus">
+            <div class="container" id="">
+                <div class="row" >
+                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <h1 style="text-align :center"  >Contacts</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
         <script type="text/javascript">
 $( document ).ready(function() {
